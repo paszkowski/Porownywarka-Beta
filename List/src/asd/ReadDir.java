af2d3e80d112ce935965be3658bcb34f0a707c5a
Bartosz P <bpaszkowski18@gmail.com>
2015-11-04 14:34:39 +0100

    New method to check if the name of the files are equal

--- List/src/asd/ReadDir.java
+++ List/src/asd/ReadDir.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 
 public class ReadDir {
    public static void main(String[] args) {
+	   
       
       Scanner input = new Scanner(System.in);
-      String[] paths;
+      String[] path1;
       String[] path2;
       
       System.out.println("First directory path: ");
       String sciezka = input.nextLine();
       System.out.println("Second directory path: ");
       String sciezka2 = input.nextLine();
+ 
       
             
       try{      
@@ -22,28 +24,47 @@ public class ReadDir {
          File file2 = new File(sciezka2);
                                  
          // array of files and directory
-         paths = file.list();
+         path1 = file.list();
          path2 = file2.list();
             
          // for each name in the path array
-         System.out.println("First Directory");
-         for(String path:paths)
+        /* System.out.println("First Directory");
+         for(String path:path1)
          {
             // prints filename and directory name
-            System.out.println(path);
+            System.out.println(path + " ");
+           
          }
          
          System.out.println("Second directory");
          for(String sciezka1:path2)
          {
             // prints filename and directory name
-            System.out.println(sciezka1);
-         }
+            System.out.println(sciezka1 + " ");
+          
+         }*/
          
+         int i;
+         int same = 0;
+         int different = 0;
+         
+        for( i = 0; i<path1.length; i++) {
+        	if (path1[i].equals(path2[i])) {
+        		same=same+1;
+        	}
+        	else {
+        		different = different + 1;
+        	}
+        
+        }
+        System.out.println("Plikow o tej samej nazwie " + same + " Plikow o roznej nazwie " + different);
          
       }catch(Exception e){
          // if any error occurs
          e.printStackTrace();
       }
+      
+      
+      
    }
 }
